4c17d1b55d3723aa5b3a28a1776081802f5ae92e
Eduardo Almeida <almeedusa@gmail.com>
2025-02-17 22:57:13 -0300

    Adicionando CORS para tratar requisições

--- router.py
+++ router.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from config import redis_db
 from model import qa_chain
@@ -6,6 +7,15 @@ import json
 
 # Inicializar FastAPI
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens (ajuste conforme necessário)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
 
 # Modelo de entrada
 class QueryRequest(BaseModel):
@@ -16,7 +26,9 @@ class QueryRequest(BaseModel):
 async def answer_query(request: QueryRequest):
     try:
         # Executar a query no chain
-        response = qa_chain.run(request.query)
+        print("Recebido:", request) 
+        response = qa_chain.invoke(request.query)
+
 
         # Salvar a pergunta e a resposta no Redis
         data = {"query": request.query, "response": response}
@@ -24,6 +36,7 @@ async def answer_query(request: QueryRequest):
 
         return data
     except Exception as e:
+        print("Erro no Backend:", str(e))  # 🔹 Exibe o erro no terminal
         raise HTTPException(status_code=500, detail=str(e))
 
 # Endpoint de teste
