8163e278758f40d3cc4c610ae9313dacbef1a38c
Eduardo Almeida <almeedusa@gmail.com>
2025-02-18 11:29:11 -0300

    Removendo comentários

--- router.py
+++ router.py
@@ -5,41 +5,36 @@ from config import redis_db
 from model import qa_chain
 import json
 
-# Inicializar FastAPI
 app = FastAPI()
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permite todas as origens (ajuste conforme necessário)
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["*"],  # Permite todos os métodos HTTP (GET, POST, etc.)
-    allow_headers=["*"],  # Permite todos os cabeçalhos
+    allow_methods=["*"],  
+    allow_headers=["*"], 
 )
 
-# Modelo de entrada
+
 class QueryRequest(BaseModel):
     query: str
 
-# Endpoint para responder perguntas
 @app.post("/duvidas")
 async def answer_query(request: QueryRequest):
     try:
-        # Executar a query no chain
         print("Recebido:", request) 
         response = qa_chain.invoke(request.query)
 
 
-        # Salvar a pergunta e a resposta no Redis
         data = {"query": request.query, "response": response}
-        redis_db.rpush("queries_responses", json.dumps(data))  # Adiciona ao final da lista
+        redis_db.rpush("queries_responses", json.dumps(data)) 
 
         return data
     except Exception as e:
-        print("Erro no Backend:", str(e))  # 🔹 Exibe o erro no terminal
+        print("Erro no Backend:", str(e)) 
         raise HTTPException(status_code=500, detail=str(e))
 
-# Endpoint de teste
 @app.get("/")
 async def root():
     return {"message": "API Funcionando!"}
